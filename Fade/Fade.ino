9e4cca2ef1157de8076f770515a1eda7bfda689b
Aaron Arce <aaronarce456@gmail.com>
2023-03-15 12:53:48 -0400

    Deleted Old Source

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -3,20 +3,7 @@
 //Program: Fade
 //Resource: https://arduinointro.com/projects/2-leds-alternate-fade-effect-using-the-for-loop
 
-/*
-  Fade
 
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
-*/
 
 int ledTen = 10;       // Pin 10 ulitlizing PWM
 int ledEleven = 11;    // Pin 11 ulitlizing PWM
