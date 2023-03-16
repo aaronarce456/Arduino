0a5f050d93888b479c018844c826b5ba056b58c3
Aaron Arce <aaronarce456@gmail.com>
2023-03-16 14:07:47 -0400

    Initial Blind Spot Monitor Commit

---
+++ UltrasonicSensor-BlindSpotMonitor/UltrasonicSensor-BlindSpotMonitor.ino
@@ -0,0 +1,18 @@
+//Programmer: Aaron Arce
+//Date: 3.16.2023
+//Program: Blind Spot Monitor
+
+/*
+ *  This program will determine distances approaching 
+ *  vehicles that are in your blind spot and turn on a warning light.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
